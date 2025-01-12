03e37ce6c8059df3b4d88ef2435a74d3ae63a87b
LMauricius <mauriciosmit100@gmail.com>
2025-01-12 21:47:34 +0100

    Moved ParamList to cpp file

--- include/Vitrae/Params/ParamList.hpp
+++ include/Vitrae/Params/ParamList.hpp
@@ -29,20 +29,7 @@ class ParamList : public dynasma::PolymorphicBase
     ParamList(ParamList const &) = default;
     ParamList(ParamList &&) = default;
 
-    inline ParamList(std::initializer_list<const ParamSpec> specs)
-    {
-        for (const auto &spec : specs) {
-            m_mappedSpecs.emplace(spec.name, spec);
-        }
-
-        for (auto [nameId, spec] : m_mappedSpecs) {
-            m_specNameIds.push_back(nameId);
-            m_specList.push_back(spec);
-        }
-
-        recalculateHash();
-    }
-
+    ParamList(std::initializer_list<const ParamSpec> specs);
     template <class ContainerT>
     ParamList(const ContainerT &specs)
         requires(std::ranges::range<ContainerT> &&
@@ -59,104 +46,33 @@ class ParamList : public dynasma::PolymorphicBase
 
         recalculateHash();
     }
-
-    inline ParamList(const StableMap<StringId, ParamSpec> &mappedSpecs) : m_mappedSpecs(mappedSpecs)
-    {
-        for (auto [nameId, spec] : m_mappedSpecs) {
-            m_specNameIds.push_back(nameId);
-            m_specList.push_back(spec);
-        }
-
-        recalculateHash();
-    }
-
-    inline ParamList(StableMap<StringId, ParamSpec> &&mappedSpecs)
-        : m_mappedSpecs(std::move(mappedSpecs))
-    {
-        for (auto [nameId, spec] : m_mappedSpecs) {
-            m_specNameIds.push_back(nameId);
-            m_specList.push_back(spec);
-        }
-
-        recalculateHash();
-    }
+    ParamList(const StableMap<StringId, ParamSpec> &mappedSpecs);
+    ParamList(StableMap<StringId, ParamSpec> &&mappedSpecs);
 
     virtual ~ParamList() = default;
 
-    ParamList &operator=(const ParamList &other)
-    {
-        m_mappedSpecs = other.m_mappedSpecs;
-        m_specNameIds = other.m_specNameIds;
-        m_specList = std::vector<ParamSpec>(other.m_specList.begin(), other.m_specList.end());
-        m_hash = other.m_hash;
-        return *this;
-    }
+    ParamList &operator=(const ParamList &other);
     ParamList &operator=(ParamList &&other) = default;
 
     /**
      * Inserts specs from other at the end of this list, if they are not already in the list
      */
-    std::size_t merge(const ParamList &other)
-    {
-        std::size_t count = 0;
-        for (const auto &spec : other.m_specList) {
-            if (m_mappedSpecs.find(spec.name) == m_mappedSpecs.end()) {
-                m_mappedSpecs.emplace(spec.name, spec);
-                m_specNameIds.push_back(spec.name);
-                m_specList.push_back(spec);
-                ++count;
-            }
-        }
-
-        recalculateHash();
-
-        return count;
-    }
+    std::size_t merge(const ParamList &other);
 
     /**
      * Inserts a spec at the end of the list if it is not already in the list
      */
-    void insert_back(const ParamSpec &spec)
-    {
-        if (m_mappedSpecs.find(spec.name) == m_mappedSpecs.end()) {
-            m_mappedSpecs.emplace(spec.name, spec);
-            m_specNameIds.push_back(spec.name);
-            m_specList.push_back(spec);
-
-            recalculateHash();
-        }
-    }
+    void insert_back(const ParamSpec &spec);
 
     /**
      * Erases the spec from the list if it exists
      */
-    void erase(const ParamSpec &spec) { erase(spec.name); }
+    void erase(const ParamSpec &spec);
 
     /**
      * Erases the spec with the given name if it exists
      */
-    void erase(const StringId &nameId)
-    {
-        if (m_mappedSpecs.erase(nameId) > 0) {
-            m_mappedSpecs.erase(nameId);
-            std::size_t ind = std::find(m_specNameIds.begin(), m_specNameIds.end(), nameId) -
-                              m_specNameIds.begin();
-            m_specNameIds.erase(m_specNameIds.begin() + ind);
-
-            // we need a new vector to avoid calling std::vector::erase (ParamSpec is not
-            // move-assignable)
-            std::vector<ParamSpec> newSpecList;
-            newSpecList.reserve(m_specList.size() - 1);
-            for (int i = 0; i < m_specList.size(); i++) {
-                if (i != ind) {
-                    newSpecList.push_back(m_specList[i]);
-                }
-            }
-            m_specList = std::move(newSpecList);
-
-            recalculateHash();
-        }
-    }
+    void erase(const StringId &nameId);
 
     /*
     Getters
@@ -176,30 +92,17 @@ class ParamList : public dynasma::PolymorphicBase
 
     inline std::size_t count() const { return m_specList.size(); }
 
-    inline bool contains(StringId nameId) const
-    {
-        return m_mappedSpecs.find(nameId) != m_mappedSpecs.end();
-    }
+    bool contains(StringId nameId) const;
 
     /*
     Comparisons
     */
 
-    bool operator==(const ParamList &other) const { return m_hash == other.m_hash; }
-    auto operator<=>(const ParamList &other) const { return m_hash <=> other.m_hash; }
+    inline bool operator==(const ParamList &other) const { return m_hash == other.m_hash; }
+    inline auto operator<=>(const ParamList &other) const { return m_hash <=> other.m_hash; }
 
   private:
-    void recalculateHash()
-    {
-        m_hash = 0;
-        for (int i = 0; i < m_specList.size(); i++) {
-            m_hash = combinedHashes<3>({{
-                m_hash,
-                std::hash<StringId>{}(m_specNameIds[i]),
-                m_specList[i].typeInfo.p_id->hash_code(),
-            }});
-        }
-    }
+    void recalculateHash();
 };
 
 inline const ParamList EMPTY_PROPERTY_LIST{};
---
+++ src/Params/ParamList.cpp
@@ -0,0 +1,123 @@
+#include "Vitrae/Params/ParamList.hpp"
+
+namespace Vitrae
+{
+
+ParamList::ParamList(std::initializer_list<const ParamSpec> specs)
+{
+    for (const auto &spec : specs) {
+        m_mappedSpecs.emplace(spec.name, spec);
+    }
+
+    for (auto [nameId, spec] : m_mappedSpecs) {
+        m_specNameIds.push_back(nameId);
+        m_specList.push_back(spec);
+    }
+
+    recalculateHash();
+}
+
+ParamList::ParamList(const StableMap<StringId, ParamSpec> &mappedSpecs) : m_mappedSpecs(mappedSpecs)
+{
+    for (auto [nameId, spec] : m_mappedSpecs) {
+        m_specNameIds.push_back(nameId);
+        m_specList.push_back(spec);
+    }
+
+    recalculateHash();
+}
+
+ParamList::ParamList(StableMap<StringId, ParamSpec> &&mappedSpecs)
+    : m_mappedSpecs(std::move(mappedSpecs))
+{
+    for (auto [nameId, spec] : m_mappedSpecs) {
+        m_specNameIds.push_back(nameId);
+        m_specList.push_back(spec);
+    }
+
+    recalculateHash();
+}
+
+ParamList &ParamList::operator=(const ParamList &other)
+{
+    m_mappedSpecs = other.m_mappedSpecs;
+    m_specNameIds = other.m_specNameIds;
+    m_specList = std::vector<ParamSpec>(other.m_specList.begin(), other.m_specList.end());
+    m_hash = other.m_hash;
+    return *this;
+}
+
+std::size_t ParamList::merge(const ParamList &other)
+{
+    std::size_t count = 0;
+    for (const auto &spec : other.m_specList) {
+        if (m_mappedSpecs.find(spec.name) == m_mappedSpecs.end()) {
+            m_mappedSpecs.emplace(spec.name, spec);
+            m_specNameIds.push_back(spec.name);
+            m_specList.push_back(spec);
+            ++count;
+        }
+    }
+
+    recalculateHash();
+
+    return count;
+}
+
+void ParamList::insert_back(const ParamSpec &spec)
+{
+    if (m_mappedSpecs.find(spec.name) == m_mappedSpecs.end()) {
+        m_mappedSpecs.emplace(spec.name, spec);
+        m_specNameIds.push_back(spec.name);
+        m_specList.push_back(spec);
+
+        recalculateHash();
+    }
+}
+
+void ParamList::erase(const ParamSpec &spec)
+{
+    erase(spec.name);
+}
+
+void ParamList::erase(const StringId &nameId)
+{
+    if (m_mappedSpecs.erase(nameId) > 0) {
+        m_mappedSpecs.erase(nameId);
+        std::size_t ind =
+            std::find(m_specNameIds.begin(), m_specNameIds.end(), nameId) - m_specNameIds.begin();
+        m_specNameIds.erase(m_specNameIds.begin() + ind);
+
+        // we need a new vector to avoid calling std::vector::erase (ParamSpec is not
+        // move-assignable)
+        std::vector<ParamSpec> newSpecList;
+        newSpecList.reserve(m_specList.size() - 1);
+        for (int i = 0; i < m_specList.size(); i++) {
+            if (i != ind) {
+                newSpecList.push_back(m_specList[i]);
+            }
+        }
+        m_specList = std::move(newSpecList);
+
+        recalculateHash();
+    }
+}
+
+bool ParamList::contains(StringId nameId) const
+{
+    return m_mappedSpecs.find(nameId) != m_mappedSpecs.end();
+}
+
+void ParamList::recalculateHash()
+{
+    m_hash = 0;
+    for (int i = 0; i < m_specList.size(); i++) {
+        m_hash = combinedHashes<3>({{
+            m_hash,
+            std::hash<StringId>{}(m_specNameIds[i]),
+            m_specList[i].typeInfo.p_id->hash_code(),
+        }});
+    }
+}
+
+} // namespace Vitrae
