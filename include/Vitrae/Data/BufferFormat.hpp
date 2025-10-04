62daa5b51fd0c72d94a726d7e8cbcbc3829dffb1
LMauricius <mauriciosmit100@gmail.com>
2025-10-04 17:52:26 +0200

    Fixed Buffer types and documentation

--- include/Vitrae/Data/BufferFormat.hpp
+++ include/Vitrae/Data/BufferFormat.hpp
@@ -5,39 +5,41 @@ namespace Vitrae
 
 /**
  * Native value type specification of image (and other) buffers.
- * These are generally taken very strictly, as they dictate not only the format of the buffer,
+ * This specifies what the values of the buffer represent.
+ * It is generally taken very strictly, as they dictate not only the format of the buffer,
  * but also the types of value retrieved after reading the buffer,
  * the operations that can be performed on the buffer,
  * and where it can be used.
  *
- * Constants are named like this: <Element type>_<Vector size>
- * The element type is the type of one component of the stored value.
- * The vector size is the number of components of the stored value.
+ * Constants are named like this: <Component type>_<Vector size>
+ * The element type is the type of one item of the stored value.
+ * The vector size is the number of items of the stored value.
  *
- * Element types:
- *  - FLOAT - A floating point value
+ * Component types:
+ *  - REAL - A real value, with a number of decimal places
  *  - INT - A signed integer value
- *  - UNSIGNED_INT - An unsigned integer value
+ *  - UNSIGNED - An unsigned integer value
  *
  * Vector sizes:
- *  - SCALAR - the value is the element, so not a vector at all
- *  - VEC2 - 2 components stored
- *  - VEC3 - 3 components stored
- *  - VEC4 - 4 components stored
+ *  - SCALAR - the value has a single component, so not a vector at all
+ *  - VEC2 - 2 components stored in the value
+ *  - VEC3 - 3 components stored in the value
+ *  - VEC4 - 4 components stored in the value
  *
  * For convenience, the following aliases are defined:
- *  - COLOR_OPAQUE - A 3 component float vector holding an RGB color
- *  - COLOR_TRANSPARENT - A 4 component float vector holding an RGBA color
+ *  - COLOR_OPAQUE - A 3 component real number vector holding an RGB color
+ *  - COLOR_TRANSPARENT - A 4 component real number vector holding an RGBA color
  *
  * There are some special types that generally require special handling:
- *  - DEPTH -  Distance of rendered geometry (a special floating point value)
+ *  - DEPTH -  Distance of rendered geometry (a special real number value)
  *  - STENCIL - A stencil value (a special integer value used for many effects)
+ *  - DEPTH_AND_STENCIL - A pair of depth and stencil values in one buffer
  */
 enum class BufferType {
-    FLOAT_SCALAR,
-    FLOAT_VEC2,
-    FLOAT_VEC3,
-    FLOAT_VEC4,
+    REAL_SCALAR,
+    REAL_VEC2,
+    REAL_VEC3,
+    REAL_VEC4,
     INT_SCALAR,
     INT_VEC2,
     INT_VEC3,
@@ -49,15 +51,16 @@ enum class BufferType {
     DEPTH,
     STENCIL,
     DEPTH_AND_STENCIL,
-    COLOR_OPAQUE = FLOAT_VEC3,
-    COLOR_TRANSPARENT = FLOAT_VEC4,
+    COLOR_OPAQUE = REAL_VEC3,
+    COLOR_TRANSPARENT = REAL_VEC4,
 };
 
 // used for specializing enums and types tied to a specific BufferType
 template <BufferType BUFFER_TYPE> struct BufferTypeSpecialization;
 
 /**
- * Storage format specification enum of image (and other) buffers.
+ * Storage format specification of image (and other) buffers.
+ * This value specified how the bits in memory represent data in our buffers
  * Each BufferType has its own format constants.
  *
  * The constants are named in the format of <Base type><Bit count>, with some exceptions.
@@ -66,27 +69,29 @@ template <BufferType BUFFER_TYPE> struct BufferTypeSpecialization;
  * each component of the vector, in which case the bit counts are given in order.
  *
  * Base types:
- *  - FLOAT - A floating point value, with the range depending on the bit count
- *  - INT - A signed integer value, with the range depending on the bit count
- *  - UNSIGNED_INT - An unsigned integer value, with the range depending on the bit count
- *  - UNORM - A value with the range [0.0, 1.0], stored as an unsigned integer but read as a float
- *  - SNORM - A value with the range [-1.0, 1.0], stored as a signed integer but read as a float
+ *  - FLOAT - Floating point value, with the range and precision depending on the bit count
+ *  - UFLOAT - Unsigned floating point value (stored without the sign bit)
+ *  - INT - Signed integer value, with the range depending on the bit count
+ *  - UNSIGNED - Unsigned integer value, with the range depending on the bit count
+ *  - UNORM - Signed normalized float value in the range [0.0, 1.0], stored as an unsigned integer
+ *  - SNORM - Unsigned normalized float value in the range [-1.0, 1.0], stored as a signed integer
+ *  - NORM - Normalized float value for types where there is no difference of unsigned or signed
  *
  * Special types:
- *  - GENERIC / GENERIC_FULL - A large ranged type, with the format left to the implementation
- *  - GENERIC_UNORM - A type with the range [0.0, 1.0], with the format left to the implementation
- *  - LINEAR_COLOR - A type with the range [0.0, 1.0], fitting for the linear color space
- *  - SRGB_COLOR - A type with the range [0.0, 1.0], fitting for the sRGB color space
+ *  - GENERIC / GENERIC_FULL - Large ranged value, with the format left to the implementation
+ *  - GENERIC_UNORM - Value in the range [0.0, 1.0], with the format left to the implementation
+ *  - BOOL - Either 1 or 0, i.e. unsigned integer with 1 bit
+ *  - LINEAR_COLOR - Value in the range [0.0, 1.0], fitting for the linear color space
+ *  - SRGB_COLOR - Value in the range [0.0, 1.0], fitting for the sRGB color space
  *  - SRGB_ALPHA_COLOR - sRGB color, with the alpha in linear color space
- *  - UFLOAT_<*> - A floating point value without the sign bit
- *  - <*>_EXP<Bits> - A vector of floating point values with a shared exponent of specified bits
+ *  - <*>_EXP<Bits> - Vector of floating point values with a shared exponent of specified bits
  *
  * @note Alternatively you can use constants from enum classes named as BufferStorage_TYPE,
  * where TYPE is the BufferType that would be passed as a template parameter
  */
 template <BufferType BT> using BufferStorage = typename BufferTypeSpecialization<BT>::BufferStorage;
 
-enum class BufferStorage_FLOAT_SCALAR {
+enum class BufferStorage_REAL_SCALAR {
     GENERIC_FULL,
     GENERIC_UNORM,
     LINEAR_COLOR,
@@ -98,7 +103,7 @@ enum class BufferStorage_FLOAT_SCALAR {
     SNORM16,
 };
 
-enum class BufferStorage_FLOAT_VEC2 {
+enum class BufferStorage_REAL_VEC2 {
     GENERIC_FULL,
     GENERIC_UNORM,
     LINEAR_COLOR,
@@ -110,7 +115,7 @@ enum class BufferStorage_FLOAT_VEC2 {
     SNORM16,
 };
 
-enum class BufferStorage_FLOAT_VEC3 {
+enum class BufferStorage_REAL_VEC3 {
     GENERIC_FULL,
     GENERIC_UNORM,
     LINEAR_COLOR,
@@ -128,11 +133,11 @@ enum class BufferStorage_FLOAT_VEC3 {
 
     UNORM_3_3_2,
     UFLOAT_11_11_10,
-    FLOAT9_EXP5,
+    UFLOAT9_EXP5,
     UNORM_5_6_5,
 };
 
-enum class BufferStorage_FLOAT_VEC4 {
+enum class BufferStorage_REAL_VEC4 {
     GENERIC_FULL,
     GENERIC_UNORM,
     LINEAR_COLOR,
@@ -233,24 +238,24 @@ enum class BufferStorage_DEPTH_AND_STENCIL {
 
 // ---- Specialization wrappers per BufferType ----
 
-template <> struct BufferTypeSpecialization<BufferType::FLOAT_SCALAR>
+template <> struct BufferTypeSpecialization<BufferType::REAL_SCALAR>
 {
-    using BufferStorage = BufferStorage_FLOAT_SCALAR;
+    using BufferStorage = BufferStorage_REAL_SCALAR;
 };
 
-template <> struct BufferTypeSpecialization<BufferType::FLOAT_VEC2>
+template <> struct BufferTypeSpecialization<BufferType::REAL_VEC2>
 {
-    using BufferStorage = BufferStorage_FLOAT_VEC2;
+    using BufferStorage = BufferStorage_REAL_VEC2;
 };
 
-template <> struct BufferTypeSpecialization<BufferType::FLOAT_VEC3>
+template <> struct BufferTypeSpecialization<BufferType::REAL_VEC3>
 {
-    using BufferStorage = BufferStorage_FLOAT_VEC3;
+    using BufferStorage = BufferStorage_REAL_VEC3;
 };
 
-template <> struct BufferTypeSpecialization<BufferType::FLOAT_VEC4>
+template <> struct BufferTypeSpecialization<BufferType::REAL_VEC4>
 {
-    using BufferStorage = BufferStorage_FLOAT_VEC4;
+    using BufferStorage = BufferStorage_REAL_VEC4;
 };
 
 template <> struct BufferTypeSpecialization<BufferType::INT_SCALAR>
