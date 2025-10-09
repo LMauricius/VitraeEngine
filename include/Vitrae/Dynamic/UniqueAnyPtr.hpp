0971708843107b048cfcb2312633dc758478e974
LMauricius <mauriciosmit100@gmail.com>
2025-10-10 01:01:48 +0200

    Performance improvements for UniqueAnyPtr

--- include/Vitrae/Dynamic/UniqueAnyPtr.hpp
+++ include/Vitrae/Dynamic/UniqueAnyPtr.hpp
@@ -15,21 +15,22 @@ class UniqueAnyPtr
 
   public:
     // constructors
-    inline UniqueAnyPtr() : mPtr(nullptr), mDeleter(nullptr) {}
-    template <class T> UniqueAnyPtr(T *ptr)
+    constexpr UniqueAnyPtr() : mPtr(nullptr), mDeleter() {}
+    constexpr UniqueAnyPtr(std::nullptr_t) : mPtr(nullptr), mDeleter() {}
+    template <class T> constexpr UniqueAnyPtr(T *ptr)
     {
         mPtr = ptr;
         mDeleter = [](void *ptr) { delete static_cast<T *>(ptr); };
     }
 
-    template <class T> UniqueAnyPtr(std::unique_ptr<T> &&ptr)
+    template <class T> constexpr UniqueAnyPtr(std::unique_ptr<T> &&ptr)
     {
         mPtr = ptr.release();
         mDeleter = [](void *ptr) { delete static_cast<T *>(ptr); };
     }
 
     // move constructor
-    inline UniqueAnyPtr(UniqueAnyPtr &&other)
+    constexpr UniqueAnyPtr(UniqueAnyPtr &&other)
     {
         mPtr = other.mPtr;
         mDeleter = other.mDeleter;
@@ -37,47 +38,46 @@ class UniqueAnyPtr
         other.mDeleter = nullptr;
     }
 
-    inline ~UniqueAnyPtr()
+    constexpr ~UniqueAnyPtr()
     {
-        if (mPtr != nullptr)
+        if (mPtr)
             (*mDeleter)(mPtr);
     }
 
     // move assignment
-    template <class T> UniqueAnyPtr &operator=(T *ptr)
+    template <class T> constexpr UniqueAnyPtr &operator=(T *ptr)
     {
-        if (mPtr != nullptr)
+        if (mPtr)
             (*mDeleter)(mPtr);
         mPtr = ptr;
         mDeleter = [](void *ptr) { delete static_cast<T *>(ptr); };
         return *this;
     }
 
-    template <class T> UniqueAnyPtr &operator=(std::unique_ptr<T> &&ptr)
+    template <class T> constexpr UniqueAnyPtr &operator=(std::unique_ptr<T> &&ptr)
     {
-        if (mPtr != nullptr)
+        if (mPtr)
             (*mDeleter)(mPtr);
         mPtr = ptr.release();
         mDeleter = [](void *ptr) { delete static_cast<T *>(ptr); };
         return *this;
     }
 
-    inline UniqueAnyPtr &operator=(UniqueAnyPtr &&other)
+    constexpr UniqueAnyPtr &operator=(UniqueAnyPtr &&other)
     {
-        if (mPtr != nullptr)
+        if (mPtr)
             (*mDeleter)(mPtr);
         mPtr = other.mPtr;
         mDeleter = other.mDeleter;
         other.mPtr = nullptr;
-        other.mDeleter = nullptr;
         return *this;
     }
 
-    inline operator bool() const { return mPtr != nullptr; }
+    constexpr operator bool() const { return mPtr != nullptr; }
 
     /**
      * @returns the raw pointer to the object or nullptr if not set
      */
-    template <class T> T *get() const { return static_cast<T *>(mPtr); }
+    template <class T> constexpr T *get() const { return static_cast<T *>(mPtr); }
 };
 } // namespace Vitrae
